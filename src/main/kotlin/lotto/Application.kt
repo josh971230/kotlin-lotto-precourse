b1413d837ee8d97e2412cca2957d7e52bbcc5f48
josh971230 <brionlee97@naver.com>
2025-04-28 23:36:55 +0900

    feat: implement Application main to orchestrate lottery flow

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,5 +1,26 @@
+
 package lotto
 
 fun main() {
-    // TODO: Implement the program
+
+    val amount = InputView.readPurchaseAmount()
+    val purchase = Purchase(amount)
+    val count = purchase.ticketCount()
+
+
+    OutputView.printPurchase(count)
+    val tickets = LottoGenerator().generate(count)
+    OutputView.printTickets(tickets)
+
+
+    val winning = InputView.readWinningNumbers()
+    val bonus = InputView.readBonusNumber(winning)
+
+
+    val evaluator = LottoResultEvaluator()
+    val results = tickets.map { evaluator.evaluate(it, winning, bonus) }
+
+
+    val stats = LottoStatistics(results, amount)
+    OutputView.printStatistics(stats)
 }
